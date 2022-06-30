3b671b5ec6500cb4a2ca453bf3a9d7898c0f9d53
kamalrossi <rossikamal@gmail.com>
2022-06-30 15:41:23 +0600

    first commit

---
+++ src/pages/about/data/projectdata.js
@@ -0,0 +1,48 @@
+const projectowner = "Rossi Kamal(rossikamal@gmail.com)";
+const projectname = "iHealth";
+const projectdesc = {
+  title: "Title",
+  desc:
+    "i-health is a responsive web app for analytics based healthcare services. The software includes major modules such as about us, service, technology, plans."
+};
+const projecttag = [
+  {
+    tag1: "ReactJS",
+    tag3: "JSX",
+    tag4: "MedTech"
+  }
+];
+const projecttime = [
+  {
+    title: "Design",
+    from: "Jan 21",
+    to: "Mar 21"
+  },
+  {
+    title: "Implementation",
+    from: "Jan 21",
+    to: "Mar 21"
+  }
+];
+
+const projectrole = [
+  {
+    title: "UI Design",
+    desc:
+      "Analyze healthcare domain and prepare requirement analysis document."
+  },
+  {
+    title: "Responsive Web development",
+    desc:
+      "Responsive HealthCare App Development: Implementation:Code required modules homescreen, service, technology, testimonial of clinical application."
+  }
+];
+
+export {
+  projectowner,
+  projectname,
+  projectdesc,
+  projectrole,
+  projecttime,
+  projecttag
+};
